4b608c0680697fd912b8122c02cf1c3229676918
jinwooahnKHU <96bluechip@khu.ac.kr>
2021-07-06 09:03:00 +0900

    Initial Commit

---
+++ Swift_tutorial/Swift_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_tutorialApp.swift
+//  Swift_tutorial
+//
+//  Created by 안진우 on 2021/07/06.
+//
+
+import SwiftUI
+
+@main
+struct Swift_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
